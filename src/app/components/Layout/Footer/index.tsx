90ecee86c593b2f3abac65c0084ae39b9b9af9ef
TONVANS <02091886915.ss@gmail.com>
2025-07-23 15:01:18 +0700

    fix icon

--- src/app/components/Layout/Footer/index.tsx
+++ src/app/components/Layout/Footer/index.tsx
@@ -158,7 +158,7 @@ const Footer = () => {
             <div>
               <p className="font-medium text-white mb-4">Address</p>
               <ul className="space-y-3 text-xm">
-                <li><p className="text-white/60"><IoLocationOutline />: ບ້ານ ທົ່ງກາງ, ເມືອງ ສີສັດຕະນາກ, ນະຄອນຫຼວງວຽງຈັນ</p></li>
+                <li><p className="text-white/60 flex items-center"><IoLocationOutline />: ບ້ານ ທົ່ງກາງ, ເມືອງ ສີສັດຕະນາກ, ນະຄອນຫຼວງວຽງຈັນ</p></li>
               </ul>
             </div>
           </div>
